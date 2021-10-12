f38c109c185680f7dd80e42e8c056c23bf6acc41
Marie Malarme <malarmemarie@gmail.com>
2021-10-12 21:47:31 +0100

    blocks: create block 4 with animated background

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { random } from './utils/toolbox'
 import { Block_1 } from './blocks/Block_1'
 import { Block_2 } from './blocks/Block_2'
 import { Block_3 } from './blocks/Block_3'
+import { Block_4 } from './blocks/Block_4'
 
 const App = () => (
   <Grid id="grid">
@@ -13,7 +14,7 @@ const App = () => (
   </Grid>
 )
 
-const blocks = [Block_1, Block_2, Block_3].map((block, index) => {
+const blocks = [Block_1, Block_2, Block_3, Block_4].map((block, index) => {
   const hue = random(0, 1000)
   const saturation = index % 2 ? random(40, 75) : random(10, 80)
   const luminosity = index % 2 ? random(40, 90) : random(15, 85)
---
+++ src/blocks/Block_4.js
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+import { Component } from '../utils/flags'
+
+export const Block_4 = (props) => {
+  const [current_direction, set_current_direction] = useState('top-right')
+  const [paused, set_paused] = useState(false)
+
+  return (
+    <Wrapper
+      className={!paused && `disco-${current_direction}`}
+      onClick={() => set_paused(!paused)}
+      style={{
+        backgroundImage: `radial-gradient(
+        yellow 0%,
+        white 49%,
+        white 51%,
+        cyan 100%)`,
+        backgroundPosition: `12.5vw 12.5vw`,
+      }}
+    >
+      <Half>
+        <Corner
+          direction="top-left"
+          current_direction={current_direction}
+          set_current_direction={set_current_direction}
+        />
+        <Corner
+          direction="top-right"
+          current_direction={current_direction}
+          set_current_direction={set_current_direction}
+        />
+      </Half>
+      <Eyes style={{ transform: `rotate(${rotations[current_direction]}deg)` }}>
+        👀
+      </Eyes>
+      <Half>
+        <Corner
+          direction="bottom-left"
+          current_direction={current_direction}
+          set_current_direction={set_current_direction}
+        />
+        <Corner
+          direction="bottom-right"
+          current_direction={current_direction}
+          set_current_direction={set_current_direction}
+        />
+      </Half>
+    </Wrapper>
+  )
+}
+
+const Corner = ({ direction, current_direction, set_current_direction }) => {
+  const top = direction.includes('top')
+  const left = direction.includes('left')
+
+  return (
+    <Quarter
+      onMouseEnter={() => set_current_direction(direction)}
+      style={{
+        alignItems: top ? 'flex-start' : 'flex-end',
+        justifyContent: left ? 'flex-start' : 'flex-end',
+      }}
+    >
+      <Icon
+        is_selected={current_direction === direction}
+        rotation={rotations[direction]}
+      />
+    </Quarter>
+  )
+}
+
+const Icon = ({ rotation, is_selected }) => (
+  <svg
+    style={{ transform: `rotate(${rotation + 180}deg)` }}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 130 130"
+    width="40px"
+  >
+    {is_selected ? Arrow : <circle cx={65.5} cy={65.5} r="15" />}
+  </svg>
+)
+
+const Arrow = (
+  <path
+    fill="none"
+    stroke="black"
+    strokeWidth={10}
+    d="m72.85 17.6 47.65 47.65-47.65 47.65M5.5 65.25h114.34"
+  />
+)
+
+const rotations = {
+  'top-right': 135,
+  'top-left': 45,
+  'bottom-right': -135,
+  'bottom-left': -45,
+}
+
+const Wrapper = Component.relative.flex.flex_column.jc_between.section()
+const Half = Component.w100p.h100p.flex.div()
+const Quarter = Component.pa30.flex.w50p.h100p.c_pointer.div()
+const Eyes =
+  Component.fs50.h100p.w100p.flex.jc_center.absolute.ai_center.events_none.div()
