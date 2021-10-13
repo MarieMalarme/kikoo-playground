bfdd766bc8d8e5da7c2da68da55e062b0749b983
Marie Malarme <malarmemarie@gmail.com>
2021-10-13 20:42:12 +0100

    blocks: create block 5 with canvas to paint on

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Block_1 } from './blocks/Block_1'
 import { Block_2 } from './blocks/Block_2'
 import { Block_3 } from './blocks/Block_3'
 import { Block_4 } from './blocks/Block_4'
+import { Block_5 } from './blocks/Block_5'
 
 const App = () => (
   <Grid id="grid">
@@ -14,13 +15,15 @@ const App = () => (
   </Grid>
 )
 
-const blocks = [Block_1, Block_2, Block_3, Block_4].map((block, index) => {
-  const hue = random(0, 1000)
-  const saturation = index % 2 ? random(40, 75) : random(10, 80)
-  const luminosity = index % 2 ? random(40, 90) : random(15, 85)
-  const color = `hsl(${hue}, ${saturation}%, ${luminosity}%)`
-  return { block, color }
-})
+const blocks = [Block_1, Block_2, Block_3, Block_4, Block_5].map(
+  (block, index) => {
+    const hue = random(0, 1000)
+    const saturation = index % 2 ? random(40, 75) : random(10, 80)
+    const luminosity = index % 2 ? random(40, 90) : random(15, 85)
+    const color = `hsl(${hue}, ${saturation}%, ${luminosity}%)`
+    return { block, color }
+  },
+)
 
 const Grid = Component.w100vw.minH100vh.div()
 
---
+++ src/blocks/Block_5.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react'
+import { Component } from '../utils/flags'
+import { random } from '../utils/toolbox'
+
+export const Block_5 = (props) => {
+  const [wrapper, set_wrapper] = useState(null)
+  const [canvas, set_canvas] = useState(null)
+  const [context, set_context] = useState(null)
+
+  useEffect(() => {
+    if (!canvas || !wrapper) return
+
+    // get & set the context of the canvas
+    const ctx = canvas.getContext('2d')
+    set_context(ctx)
+
+    const { width, height, top, left } = wrapper.getBoundingClientRect()
+
+    // draw a curve on the canvas on load
+    let start = { x: width / 10, y: height / 4 }
+    const steps = (width / 5).toFixed()
+    const points = [...Array(Number(steps)).keys()]
+
+    points.forEach((index) => {
+      start.x += random(0, 20)
+      start.y += random(-10, 15)
+
+      draw_point({
+        x: left + start.x,
+        y: top + start.y,
+        context: ctx,
+        wrapper,
+      })
+    })
+
+    // event listener to clear the canvas on click
+    const clear_canvas = () => ctx.clearRect(0, 0, width, height)
+    canvas.addEventListener('click', clear_canvas)
+    return () => canvas.removeEventListener('click', clear_canvas)
+  }, [canvas, wrapper])
+
+  return (
+    <Wrapper elemRef={set_wrapper} {...props}>
+      <canvas
+        ref={set_canvas}
+        width={wrapper?.getBoundingClientRect().width}
+        height={wrapper?.getBoundingClientRect().height}
+        onMouseMove={(event) =>
+          draw_point({ x: event.pageX, y: event.pageY, context, wrapper })
+        }
+      />
+    </Wrapper>
+  )
+}
+
+const draw_point = ({ x, y, context, wrapper }) => {
+  if (!context) return
+
+  const { top, left } = wrapper.getBoundingClientRect()
+
+  // set the coordinates according to the position of the canvas' wrapper in the page
+  x = x - left
+  y = y - top - window.pageYOffset
+
+  context.fillStyle = 'orange'
+  context.beginPath()
+  context.rect(x - half_point, y - half_point, point_size, point_size)
+  context.fill()
+}
+
+const point_size = 20
+const half_point = point_size / 2
+const Wrapper = Component.of_hidden.blend_exclusion.section()
