f48bb6292276627ec1e64ba86116a0c94ed7a296
Marie Malarme <malarmemarie@gmail.com>
2021-10-14 22:08:18 +0100

    block 3: add icons minus & plus

--- src/blocks/Block_3.js
+++ src/blocks/Block_3.js
@@ -16,13 +16,34 @@ export const Block_3 = (props) => {
         backgroundSize: `calc(25vw / ${tiles}) calc(25vw / ${tiles})`,
       }}
     >
-      <Button onClick={() => tiles > 1 && set_tiles(tiles - 1)}>-</Button>
+      <Button onClick={() => tiles > 1 && set_tiles(tiles - 1)}>
+        <Icon minus />
+      </Button>
       <Tiles>{tiles}</Tiles>
-      <Button onClick={() => tiles < 10 && set_tiles(tiles + 1)}>+</Button>
+      <Button onClick={() => tiles < 10 && set_tiles(tiles + 1)}>
+        <Icon plus />
+      </Button>
     </Wrapper>
   )
 }
 
+const Icon = ({ plus, minus }) => (
+  <svg
+    width="70%"
+    height="70%"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 130 130"
+  >
+    <path
+      fill="none"
+      stroke="black"
+      stroke-width={22}
+      d={(plus && 'M65 15v100M115 65H15') || (minus && 'M115 65H15')}
+    />
+  </svg>
+)
+
 const Wrapper = Component.flex.pa30.ai_center.jc_center.fs50.no_select.section()
-const Button = Component.c_pointer.div()
+const Button =
+  Component.w30.h30.ba.bw3.fs30.b_rad50p.flex.ai_center.jc_center.c_pointer.div()
 const Tiles = Component.w150.text_center.div()
