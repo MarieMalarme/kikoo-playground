e0b229af020b5dd3d69c46e21d9f2efb3924c8e1
Marie Malarme <malarmemarie@gmail.com>
2021-10-15 13:10:59 +0100

    blocks: create a file indexing all the blocks' components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import { Component } from './utils/flags'
 import { random } from './utils/toolbox'
-import { Block_1 } from './blocks/Block_1'
-import { Block_2 } from './blocks/Block_2'
-import { Block_3 } from './blocks/Block_3'
-import { Block_4 } from './blocks/Block_4'
-import { Block_5 } from './blocks/Block_5'
-import { Block_6 } from './blocks/Block_6'
-import { Block_7 } from './blocks/Block_7'
-import { Block_8 } from './blocks/Block_8'
-import { Block_9 } from './blocks/Block_9'
+import { blocks_list } from './blocks/blocks.js'
 
 const App = () => (
   <Grid id="grid">
@@ -21,17 +13,8 @@ const App = () => (
   </Grid>
 )
 
-const blocks = [
-  Block_1,
-  Block_2,
-  Block_3,
-  Block_4,
-  Block_5,
-  Block_6,
-  Block_7,
-  Block_8,
-  Block_9,
-].map((block, index) => {
+// assign a random color to each block
+const blocks = blocks_list.map((block, index) => {
   const hue = random(0, 360)
   const saturation = index % 2 ? random(40, 75) : random(10, 80)
   const luminosity = index % 2 ? random(40, 90) : random(15, 85)
---
+++ src/blocks/blocks.js
@@ -0,0 +1,21 @@
+import { Block_1 } from './Block_1'
+import { Block_2 } from './Block_2'
+import { Block_3 } from './Block_3'
+import { Block_4 } from './Block_4'
+import { Block_5 } from './Block_5'
+import { Block_6 } from './Block_6'
+import { Block_7 } from './Block_7'
+import { Block_8 } from './Block_8'
+import { Block_9 } from './Block_9'
+
+export const blocks_list = [
+  Block_1,
+  Block_2,
+  Block_3,
+  Block_4,
+  Block_5,
+  Block_6,
+  Block_7,
+  Block_8,
+  Block_9,
+]
