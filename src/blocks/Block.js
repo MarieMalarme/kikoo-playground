ec434f5d9ce19924681560ef76ba1984787f20bc
Marie Malarme <malarmemarie@gmail.com>
2021-11-04 20:26:24 +0100

    source code: display source code on click on tags icon

--- src/blocks/Block.js
+++ src/blocks/Block.js
@@ -1,17 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Component } from '../utils/flags'
-import { Code, Downsize, Upsize } from '../icons'
+import { Tags, Downsize, Upsize } from '../icons'
 
-export const Block = ({ block, index, selected_block, ...states }) => {
+export const Block = ({ block, index, ...states }) => {
+  const [display_code, set_display_code] = useState(false)
   const [hovered, set_hovered] = useState(false)
-  const is_selected = selected_block === index + 1
 
   const { component, color } = block
   const Component = component
 
+  const { selected_block, set_selected_block } = states
+  const { scroll_top, set_scroll_top } = states
+  const is_selected = selected_block === index + 1
+
+  const fullscreen = {
+    enter: () => {
+      set_scroll_top(window.pageYOffset)
+      set_selected_block(index + 1)
+      set_hovered(true)
+    },
+    exit: async () => {
+      await set_selected_block(null)
+      set_hovered(false)
+      window.scrollTo(0, scroll_top)
+    },
+  }
+
   return (
     <Section
       name={index + 1}
+      flex={is_selected}
       w100vw={is_selected}
       h100vh={is_selected}
       none={selected_block && !is_selected}
@@ -21,46 +39,79 @@ export const Block = ({ block, index, selected_block, ...states }) => {
       style={{ '--color-block': color.value }}
     >
       <Component color={color} is_selected={is_selected} hovered={hovered} />
+
       {hovered && (
         <Header>
-          <Toggle>
-            <Code width={19} stroke_width={8} />
-          </Toggle>
+          <DisplayCode
+            display_code={display_code}
+            set_display_code={set_display_code}
+            is_selected={is_selected}
+            fullscreen={fullscreen}
+          />
           <Fullscreen
             index={index}
             is_selected={is_selected}
-            states={{ ...states, set_hovered }}
+            fullscreen={fullscreen}
           />
         </Header>
       )}
+
+      {display_code && is_selected && <SourceCode index={index} />}
     </Section>
   )
 }
 
-const Fullscreen = ({ index, is_selected, states }) => {
-  const { set_selected_block, scroll_top, set_scroll_top, set_hovered } = states
-  const Icon = is_selected ? Downsize : Upsize
+const DisplayCode = (props) => {
+  const { display_code, set_display_code, is_selected, fullscreen } = props
 
-  const enter = () => {
-    set_scroll_top(window.pageYOffset)
-    set_selected_block(index + 1)
-    set_hovered(true)
-  }
-
-  const exit = async () => {
-    await set_selected_block(null)
-    set_hovered(false)
-    window.scrollTo(0, scroll_top)
+  const toggle_display_code = () => {
+    if (!is_selected) {
+      set_display_code(true)
+      fullscreen.enter()
+    } else {
+      set_display_code(!display_code)
+    }
   }
 
   return (
-    <Toggle onClick={is_selected ? exit : enter}>
+    <Toggle onClick={toggle_display_code}>
+      <Tags width={19} stroke_width={8} />
+    </Toggle>
+  )
+}
+
+const Fullscreen = ({ index, is_selected, fullscreen }) => {
+  const Icon = is_selected ? Downsize : Upsize
+
+  return (
+    <Toggle onClick={is_selected ? fullscreen.exit : fullscreen.enter}>
       <Icon width={13} stroke_width={10} stroke="var(--grey6)" />
     </Toggle>
   )
 }
 
+const SourceCode = ({ index }) => {
+  const [source_code, set_source_code] = useState()
+  const source_code_url = `${github_url}/Block_${index + 1}.js`
+
+  useEffect(() => {
+    const fetch_source_code = async () => {
+      const response = await fetch(source_code_url)
+      const text = await response.text()
+      set_source_code(text)
+    }
+
+    fetch_source_code()
+  }, [source_code_url])
+
+  return <Code>{source_code}</Code>
+}
+
+const github_url =
+  'https://raw.githubusercontent.com/MarieMalarme/kikoo-playground/master/src/blocks/'
+
 const Section = Component.section()
 const Header = Component.flex.zi10.absolute.r10.t10.header()
 const Toggle =
-  Component.outline_none.ai_center.flex.jc_center.shadow_a_s.c_pointer.bg_white.ml10.w25.h25.ba0.b_rad5.button()
+  Component.ol_none.flex.ai_center.jc_center.shadow_a_s.c_pointer.bg_white.ml10.w25.h25.ba0.b_rad5.button()
+const Code = Component.mono.fs14.of_scroll.ws_pre.pv30.ph35.code()
