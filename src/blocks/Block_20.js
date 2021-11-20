9f11e654131ff328c10744789d86edeb38dc469e
Marie Malarme <malarmemarie@gmail.com>
2021-11-20 23:35:50 +0000

    block 20: handle touch events

--- src/blocks/Block_20.js
+++ src/blocks/Block_20.js
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Component } from '../utils/flags'
 
 export const Block_20 = ({ is_selected }) => {
   const [time, set_time] = useState('pm')
+  const [touched, set_touched] = useState()
   const [mouse_y, set_mouse_y] = useState(135)
   const [wrapper, set_wrapper] = useState(null)
 
@@ -12,12 +13,22 @@ export const Block_20 = ({ is_selected }) => {
   const weight = translate_to_scale(mouse_y / height, weight_diff) + min_weight
   const top = -translate_to_scale(mouse_y / height, height) + height / 2 || 0
 
+  const handle_wheel = (event) => {
+    event = event.type === 'touchmove' ? event.changedTouches[0] : event
+    set_mouse_y(event.pageY - wrapper.offsetParent.offsetTop)
+  }
+
+  useEffect(() => {
+    document.body.style.overflow = touched ? 'hidden' : 'auto'
+  }, [touched])
+
   return (
     <Wrapper
       elemRef={set_wrapper}
-      onMouseMove={(event) => {
-        set_mouse_y(event.pageY - wrapper.offsetParent.offsetTop)
-      }}
+      onTouchStart={() => set_touched(true)}
+      onTouchEnd={() => set_touched()}
+      onTouchMove={handle_wheel}
+      onMouseMove={handle_wheel}
       style={{ background: `hsl(230, 50%, ${luminosity}%)` }}
     >
       <Period fs100={is_selected} period="pm" time={time} set_time={set_time} />
@@ -54,7 +65,8 @@ const min_weight = 100
 const weight_diff = max_weight - min_weight
 
 const Wrapper = Component.flex.flex_column.ai_center.jc_between.article()
-const Element = Component.events_none.absolute.lh22vw.fs20vw.sun_moon.div()
+const Element =
+  Component.events_none.absolute.lh22vw.fs20vw.fs200__xs.lh300__xs.sun_moon.div()
 const Div = Component.ph30.h50p.w100p.div()
 const Text =
   Component.ba.h100p.flex.ai_center.jc_center.w100p.b_white.bw3.white.uppercase.fs70.text_center.ls5.p()
