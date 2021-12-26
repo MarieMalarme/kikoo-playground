a250b2906d515eb1baeb034be38ee077d3cede21
Marie Malarme <malarmemarie@gmail.com>
2021-12-26 20:17:55 +0100

    blocks 16 & 20: switch order

--- src/blocks/Block_16.js
+++ src/blocks/Block_16.js
@@ -1,112 +1,58 @@
 import { useState, useEffect } from 'react'
 import { Component } from '../utils/flags'
-import { random } from '../utils/toolbox'
 
-import cat_1_1 from '../images/cat-1-1.jpg'
-import cat_1_2 from '../images/cat-1-2.jpg'
-import cat_1_3 from '../images/cat-1-3.jpg'
-import cat_1_4 from '../images/cat-1-4.jpg'
-
-import cat_2_1 from '../images/cat-2-1.jpg'
-import cat_2_2 from '../images/cat-2-2.jpg'
-import cat_2_3 from '../images/cat-2-3.jpg'
-import cat_2_4 from '../images/cat-2-4.jpg'
-
-import cat_3_1 from '../images/cat-3-1.jpg'
-import cat_3_2 from '../images/cat-3-2.jpg'
-import cat_3_3 from '../images/cat-3-3.jpg'
-import cat_3_4 from '../images/cat-3-4.jpg'
-
-import cat_4_1 from '../images/cat-4-1.jpg'
-import cat_4_2 from '../images/cat-4-2.jpg'
-import cat_4_3 from '../images/cat-4-3.jpg'
-import cat_4_4 from '../images/cat-4-4.jpg'
-
-const limit = 50
-
-export const Block_16 = () => {
-  const [selected_cat_index, set_selected_cat_index] = useState(0)
-  const [mouse, set_mouse] = useState({ x: 0, y: 0 })
+export const Block_16 = ({ color, is_selected }) => {
   const [wrapper, set_wrapper] = useState(null)
-  const [{ width, height }, set_dimensions] = useState({ width: 0, height: 0 })
+  const [mouse_y, set_mouse_y] = useState(0)
 
-  useEffect(() => {
-    if (!wrapper) return
-    set_dimensions(wrapper.getBoundingClientRect())
-    set_mouse({
-      x: random(limit, width - limit),
-      y: random(limit, height - limit),
-    })
+  const height = wrapper?.getBoundingClientRect().height
 
-    const resizeObserver = new ResizeObserver(() => {
-      set_dimensions(wrapper.getBoundingClientRect())
-      set_mouse({
-        x: random(limit, width - limit),
-        y: random(limit, height - limit),
-      })
-    })
-    resizeObserver.observe(wrapper)
-    return () => resizeObserver.disconnect()
-  }, [wrapper, height, width])
+  const luminosity = translate_to_scale(mouse_y / height, 70)
+  const weight = translate_to_scale(mouse_y / height, weight_diff) + min_weight
+  const top = -translate_to_scale(mouse_y / height, height) + height / 2 || 0
+
+  const handle_wheel = (event) => {
+    event = event.type === 'touchmove' ? event.touches[0] : event
+    set_mouse_y(event.pageY - wrapper.offsetParent.offsetTop)
+  }
 
   useEffect(() => {
     const prevent_scroll = (event) => event.preventDefault()
     if (!wrapper) return
+    set_mouse_y(wrapper.getBoundingClientRect().height / 2)
+
     wrapper.addEventListener('touchmove', prevent_scroll, { passive: false })
+    return () =>
+      wrapper.removeEventListener('touchmove', prevent_scroll, {
+        passive: false,
+      })
   }, [wrapper])
-
-  const update_mouse = (event) => {
-    event = event.type === 'touchmove' ? event.touches[0] : event
-    const { offsetTop, offsetLeft } = wrapper.offsetParent
-    const client_x = event.clientX - offsetLeft
-    const client_y = event.clientY - (offsetTop - window.pageYOffset)
-
-    const allowed_x = client_x > limit && width - client_x > limit
-    const allowed_y = client_y > limit && height - client_y > limit
-
-    if (allowed_x && allowed_y) set_mouse({ x: client_x, y: client_y })
-    if (allowed_x && !allowed_y) set_mouse({ ...mouse, x: client_x })
-    if (allowed_y && !allowed_x) set_mouse({ ...mouse, y: client_y })
-  }
 
   return (
     <Wrapper
       elemRef={set_wrapper}
-      onMouseMove={update_mouse}
-      onTouchMove={update_mouse}
+      onTouchMove={handle_wheel}
+      onMouseMove={handle_wheel}
+      style={{ background: `hsl(${color.hue}, 50%, ${luminosity}%)` }}
     >
-      {wrapper &&
-        cats[selected_cat_index].map((image, index) => (
-          <Image
-            key={index}
-            src={image}
-            b0={index >= 2}
-            t0={index <= 1}
-            r0={index % 2}
-            l0={!(index % 2)}
-            width={index % 2 ? width - mouse.x : mouse.x}
-            height={index <= 1 ? mouse.y : height - mouse.y}
-          />
-        ))}
-      <Button
-        onClick={() => {
-          const is_last_index = selected_cat_index === cats.length - 1
-          const next_index = is_last_index ? 0 : selected_cat_index + 1
-          set_selected_cat_index(next_index)
-        }}
+      <Element
+        fs30vw={is_selected}
+        lh100vh={is_selected}
+        style={{ fontVariationSettings: `"wght" ${weight}`, top }}
       >
-        Another one!
-      </Button>
+        O
+      </Element>
     </Wrapper>
   )
 }
 
-const cat_1 = [cat_1_1, cat_1_2, cat_1_3, cat_1_4]
-const cat_2 = [cat_2_1, cat_2_2, cat_2_3, cat_2_4]
-const cat_3 = [cat_3_1, cat_3_2, cat_3_3, cat_3_4]
-const cat_4 = [cat_4_1, cat_4_2, cat_4_3, cat_4_4]
-const cats = [cat_1, cat_2, cat_3, cat_4]
+const translate_to_scale = (number, max) =>
+  Math.round(Math.min(max, Math.max(0, max * number)))
 
-const Wrapper = Component.flex.flex_wrap.ai_center.jc_center.article()
-const Button = Component.absolute.t20.mono.fs12.pv5.ph10.as_center.button()
-const Image = Component.ma0.img()
+const max_weight = 300
+const min_weight = 100
+const weight_diff = max_weight - min_weight
+
+const Wrapper = Component.flex.flex_column.ai_center.jc_between.article()
+const Element =
+  Component.events_none.absolute.lh22vw.fs20vw.fs200__xs.lh300__xs.sun_moon.div()
--- src/blocks/Block_20.js
+++ src/blocks/Block_20.js
@@ -1,21 +1,53 @@
 import { useState, useEffect } from 'react'
 import { Component } from '../utils/flags'
+import { random } from '../utils/toolbox'
 
-export const Block_20 = ({ is_selected }) => {
-  const [time, set_time] = useState('pm')
-  const [mouse_y, set_mouse_y] = useState(135)
+import cat_1_1 from '../images/cat-1-1.jpg'
+import cat_1_2 from '../images/cat-1-2.jpg'
+import cat_1_3 from '../images/cat-1-3.jpg'
+import cat_1_4 from '../images/cat-1-4.jpg'
+
+import cat_2_1 from '../images/cat-2-1.jpg'
+import cat_2_2 from '../images/cat-2-2.jpg'
+import cat_2_3 from '../images/cat-2-3.jpg'
+import cat_2_4 from '../images/cat-2-4.jpg'
+
+import cat_3_1 from '../images/cat-3-1.jpg'
+import cat_3_2 from '../images/cat-3-2.jpg'
+import cat_3_3 from '../images/cat-3-3.jpg'
+import cat_3_4 from '../images/cat-3-4.jpg'
+
+import cat_4_1 from '../images/cat-4-1.jpg'
+import cat_4_2 from '../images/cat-4-2.jpg'
+import cat_4_3 from '../images/cat-4-3.jpg'
+import cat_4_4 from '../images/cat-4-4.jpg'
+
+const limit = 50
+
+export const Block_20 = () => {
+  const [selected_cat_index, set_selected_cat_index] = useState(0)
+  const [mouse, set_mouse] = useState({ x: 0, y: 0 })
   const [wrapper, set_wrapper] = useState(null)
+  const [{ width, height }, set_dimensions] = useState({ width: 0, height: 0 })
 
-  const height = wrapper?.getBoundingClientRect().height
+  useEffect(() => {
+    if (!wrapper) return
+    set_dimensions(wrapper.getBoundingClientRect())
+    set_mouse({
+      x: random(limit, width - limit),
+      y: random(limit, height - limit),
+    })
 
-  const luminosity = translate_to_scale(mouse_y / height, 70)
-  const weight = translate_to_scale(mouse_y / height, weight_diff) + min_weight
-  const top = -translate_to_scale(mouse_y / height, height) + height / 2 || 0
-
-  const handle_wheel = (event) => {
-    event = event.type === 'touchmove' ? event.touches[0] : event
-    set_mouse_y(event.pageY - wrapper.offsetParent.offsetTop)
-  }
+    const resizeObserver = new ResizeObserver(() => {
+      set_dimensions(wrapper.getBoundingClientRect())
+      set_mouse({
+        x: random(limit, width - limit),
+        y: random(limit, height - limit),
+      })
+    })
+    resizeObserver.observe(wrapper)
+    return () => resizeObserver.disconnect()
+  }, [wrapper, height, width])
 
   useEffect(() => {
     const prevent_scroll = (event) => event.preventDefault()
@@ -23,49 +55,58 @@ export const Block_20 = ({ is_selected }) => {
     wrapper.addEventListener('touchmove', prevent_scroll, { passive: false })
   }, [wrapper])
 
+  const update_mouse = (event) => {
+    event = event.type === 'touchmove' ? event.touches[0] : event
+    const { offsetTop, offsetLeft } = wrapper.offsetParent
+    const client_x = event.clientX - offsetLeft
+    const client_y = event.clientY - (offsetTop - window.pageYOffset)
+
+    const allowed_x = client_x > limit && width - client_x > limit
+    const allowed_y = client_y > limit && height - client_y > limit
+
+    if (allowed_x && allowed_y) set_mouse({ x: client_x, y: client_y })
+    if (allowed_x && !allowed_y) set_mouse({ ...mouse, x: client_x })
+    if (allowed_y && !allowed_x) set_mouse({ ...mouse, y: client_y })
+  }
+
   return (
     <Wrapper
       elemRef={set_wrapper}
-      onTouchMove={handle_wheel}
-      onMouseMove={handle_wheel}
-      style={{ background: `hsl(230, 50%, ${luminosity}%)` }}
+      onMouseMove={update_mouse}
+      onTouchMove={update_mouse}
     >
-      <Period fs100={is_selected} period="pm" time={time} set_time={set_time} />
-      <Element
-        fs30vw={is_selected}
-        lh100vh={is_selected}
-        style={{ fontVariationSettings: `"wght" ${weight}`, top }}
+      {wrapper &&
+        cats[selected_cat_index].map((image, index) => (
+          <Image
+            key={index}
+            src={image}
+            b0={index >= 2}
+            t0={index <= 1}
+            r0={index % 2}
+            l0={!(index % 2)}
+            width={index % 2 ? width - mouse.x : mouse.x}
+            height={index <= 1 ? mouse.y : height - mouse.y}
+          />
+        ))}
+      <Button
+        onClick={() => {
+          const is_last_index = selected_cat_index === cats.length - 1
+          const next_index = is_last_index ? 0 : selected_cat_index + 1
+          set_selected_cat_index(next_index)
+        }}
       >
-        O
-      </Element>
-      <Period fs100={is_selected} period="am" time={time} set_time={set_time} />
+        Another one!
+      </Button>
     </Wrapper>
   )
 }
 
-const Period = ({ period, time, set_time, ...style }) => (
-  <Div
-    pt30={period === 'pm'}
-    pb30={period === 'am'}
-    onMouseEnter={() => set_time(period)}
-    className={time !== period && 'block-20 text'}
-  >
-    <Text bt0={period === 'am'} {...style}>
-      <span>{period}</span>
-    </Text>
-  </Div>
-)
+const cat_1 = [cat_1_1, cat_1_2, cat_1_3, cat_1_4]
+const cat_2 = [cat_2_1, cat_2_2, cat_2_3, cat_2_4]
+const cat_3 = [cat_3_1, cat_3_2, cat_3_3, cat_3_4]
+const cat_4 = [cat_4_1, cat_4_2, cat_4_3, cat_4_4]
+const cats = [cat_1, cat_2, cat_3, cat_4]
 
-const translate_to_scale = (number, max) =>
-  Math.round(Math.min(max, Math.max(0, max * number)))
-
-const max_weight = 300
-const min_weight = 100
-const weight_diff = max_weight - min_weight
-
-const Wrapper = Component.flex.flex_column.ai_center.jc_between.article()
-const Element =
-  Component.events_none.absolute.lh22vw.fs20vw.fs200__xs.lh300__xs.sun_moon.div()
-const Div = Component.ph30.h50p.w100p.div()
-const Text =
-  Component.ba.h100p.flex.ai_center.jc_center.w100p.b_white.bw3.white.uppercase.fs70.text_center.ls5.p()
+const Wrapper = Component.flex.flex_wrap.ai_center.jc_center.article()
+const Button = Component.absolute.t20.mono.fs12.pv5.ph10.as_center.button()
+const Image = Component.ma0.img()
