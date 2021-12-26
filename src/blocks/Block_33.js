c532ddc78795ab12ca1f97e3606560bbd272aa53
Marie Malarme <malarmemarie@gmail.com>
2021-12-26 20:30:09 +0100

    block 33: skew emojis on mouse move

---
+++ src/blocks/Block_33.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { Component } from '../utils/flags'
+
+export const Block_33 = ({ color }) => {
+  const [wrapper, set_wrapper] = useState(null)
+  const [mouse, set_mouse] = useState()
+
+  const update_mouse = (event) => {
+    event = event.type === 'touchmove' ? event.touches[0] : event
+    set_mouse({
+      x: event.pageX - wrapper.offsetParent.offsetLeft,
+      y: event.pageY - wrapper.offsetParent.offsetTop,
+    })
+  }
+
+  const coords = mouse
+    ? `${100 + mouse.x / 6}deg, ${60 + mouse.y / 8}deg`
+    : '295deg, 50deg'
+
+  return (
+    <Wrapper elemRef={set_wrapper} onMouseMove={update_mouse}>
+      {[...Array(4).keys()].map((index) => (
+        <Word
+          key={index}
+          style={{ color: color.value, transform: `skew(${coords})` }}
+        >
+          ❤️‍🔥❤️‍🔥❤️‍🔥
+        </Word>
+      ))}
+    </Wrapper>
+  )
+}
+
+const Wrapper = Component.flex.ai_center.jc_center.fs100.uppercase.article()
+const Word = Component.blend_exclusion.div()
