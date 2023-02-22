e96c8c4c46df31012dd4057be3322e0403686234
Marie Malarme <malarmemarie@gmail.com>
2023-02-22 12:57:09 +0100

    blocks: switch blocks order

--- src/blocks/Block_23.js
+++ src/blocks/Block_23.js
@@ -1,156 +1,205 @@
-import { Fragment, useState, useEffect } from 'react'
-import { Component } from '../utils/flags'
-import sk_files from '../data/sk_files.json'
+import { useState, useEffect } from 'react'
+import { Component, Div } from '../utils/flags'
+import landscape from '../images/landscape.jpeg'
+import glitch from '../images/glitch.png'
 
-export const Block_23 = ({ color, is_selected }) => {
+export const Block_23 = ({ color }) => {
   const [wrapper, set_wrapper] = useState(null)
-  const [dimensions, set_dimensions] = useState()
+
+  const [canvas, set_canvas] = useState(null)
+  const [context, set_context] = useState(null)
+
+  const [prev_mouse, set_prev_mouse] = useState(0)
+  const [tool_size, set_tool_size] = useState(50)
+  const [blend_mode, set_blend_mode] = useState('exclusion')
+
+  const [downloading, set_downloading] = useState(false)
+
+  const offset = tool_size / 2
+
+  const wrapper_width = wrapper?.getBoundingClientRect().width
+  const wrapper_height = wrapper && wrapper.getBoundingClientRect().height + 1
+
+  const draw_image = (image, context, has_blend_mode) => {
+    const img = new Image()
+    img.onload = () => {
+      context.clearRect(0, 0, wrapper_width, wrapper_height)
+
+      if (has_blend_mode) {
+        context.clearRect(0, 0, wrapper_width, wrapper_height)
+        context.fillStyle = color.value
+        context.fillRect(0, 0, wrapper_width, wrapper_height)
+        context.globalCompositeOperation = blend_mode
+      }
+
+      context.drawImage(img, 0, 0, wrapper_width, wrapper_height)
+    }
+    img.src = image
+    return img
+  }
+
+  const download = () => {
+    set_downloading(true)
+
+    const img = draw_image(canvas.toDataURL(), context, true)
+
+    setTimeout(() => {
+      const link = document.createElement('a')
+      const image = canvas
+        .toDataURL('image/png')
+        .replace('image/png', 'image/octet-stream')
+      link.href = image
+      const date = new Date().toISOString().split('T')[0]
+      link.setAttribute('download', `masterpiece_${date}.png`)
+      link.click()
+
+      context.clearRect(0, 0, wrapper_width, wrapper_height)
+      context.globalCompositeOperation = 'source-over'
+      context.drawImage(img, 0, 0, wrapper_width, wrapper_height)
+
+      set_downloading(false)
+    }, 1000)
+  }
 
   useEffect(() => {
-    if (!wrapper) return
-    set_dimensions(wrapper.getBoundingClientRect())
-  }, [wrapper, is_selected])
+    if (!canvas) return
+    const context = canvas.getContext('2d')
+    set_context(context)
+
+    const img = new Image()
+    img.onload = () => {
+      context.drawImage(img, 0, 0, wrapper_width, wrapper_height)
+    }
+    img.src = glitch
+
+    document.getElementById('section-36').style.overflow = 'visible'
+    document.getElementById('block-36').style.overflow = 'visible'
+  }, [canvas, wrapper_width, wrapper_height])
 
   return (
-    <Wrapper pa50={is_selected} elemRef={set_wrapper}>
-      {dimensions && (
-        <KillersDataviz
-          data={sk_files}
-          dimensions={dimensions}
-          color={color.value}
-          is_selected={is_selected}
-        />
+    <Wrapper id="block-36" elemRef={set_wrapper}>
+      <Canvas
+        o0={downloading}
+        style={{ mixBlendMode: blend_mode }}
+        onMouseMove={(event) => {
+          const { clientX, clientY } = event
+          const { offsetLeft, offsetTop } = wrapper.offsetParent
+          const x = clientX - offsetLeft - offset
+          const y = clientY - (offsetTop - window.pageYOffset) - offset
+
+          const pixels = context.getImageData(x, y, tool_size, tool_size)
+          const x_inc = event.clientX > prev_mouse.x ? 1 : -1
+          const y_inc = event.clientY > prev_mouse.y ? 1 : -1
+          context.putImageData(pixels, x + x_inc, y + y_inc)
+
+          set_prev_mouse({ x: event.clientX, y: event.clientY })
+        }}
+        width={wrapper_width}
+        height={wrapper_height + 1}
+        elemRef={set_canvas}
+      />
+
+      {downloading && (
+        <Downloading style={{ color: color.value }}>
+          Downloading
+          <br /> your masterpiece!
+        </Downloading>
       )}
-      <Title b80={is_selected} l80={is_selected} fs4vw={is_selected}>
-        Amount of
-        <br />
-        active serial killers
-        <br />
-        per year in the world
-      </Title>
+
+      <Controls>
+        <SelectInput blend_mode={blend_mode} set_blend_mode={set_blend_mode} />
+
+        <Input
+          value={tool_size}
+          onInput={(event) => set_tool_size(event.target.value)}
+          type="range"
+          min={20}
+          max={150}
+        />
+
+        <Button relative mb10>
+          Import image
+          <Label>
+            <input
+              onChange={(event) => {
+                const reader = new FileReader()
+                reader.onload = (event) => {
+                  draw_image(event.target.result, context)
+                }
+                reader.readAsDataURL(event.target.files[0])
+              }}
+              type="file"
+            />
+          </Label>
+        </Button>
+
+        <Button mb10 onClick={download}>
+          Download image
+        </Button>
+
+        <Button onClick={() => draw_image(landscape, context)}>
+          Reset image
+        </Button>
+      </Controls>
     </Wrapper>
   )
 }
 
-const KillersDataviz = ({ data, dimensions, color, is_selected }) => {
-  // get all the indexed active years in the data
-  const years_index = data
-    .map((killer) => killer.active_years.map((year) => year))
-    .flat()
-
-  const oldest_year = Math.min(...years_index)
-
-  // create a range covering each year of the period
-  // from the oldest year found in the data to today's current year
-  const years_range = Array(current_year - oldest_year + 1)
-    .fill(oldest_year)
-    .map((min, i) => min + i)
-
-  // create an object with all the years of the range
-  // with the year as the key & the killers' list as the value
-  const killers_per_year = Object.fromEntries(
-    years_range.map((year) => {
-      const year_killers = data.filter((killer) => {
-        const [activity_beginning, activity_end] = killer.active_years
-        return year >= activity_beginning && year <= activity_end
-      })
-      return [year, year_killers]
-    }),
-  )
-
-  const max_killers_per_year = Math.max(
-    ...Object.values(killers_per_year).map((killers) => killers.length),
-  )
-
-  // find the year with the highest amount of active killers
-  // to set is as the default hovered line
-  const highest_killers_year = Object.entries(killers_per_year).find(
-    ([year, killers]) => killers.length === max_killers_per_year,
-  )
-
-  const [hovered_line, set_hovered_line] = useState({
-    year: highest_killers_year[0],
-    killers: highest_killers_year[1],
-  })
-
-  const { width, height } = dimensions
-  const year_line_chunk = width / (years_range.length - 1)
+const SelectInput = ({ blend_mode, set_blend_mode }) => {
+  const [is_open, set_is_open] = useState(false)
 
   return (
-    <Svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${width} ${height}`}>
-      <Year textAnchor="start" fill={color} x={0} y={-10}>
-        {oldest_year}
-      </Year>
-      <Year textAnchor="end" fill={color} x={width} y={-10}>
-        {current_year}
-      </Year>
-      {Object.entries(killers_per_year).map(([year, killers], i) => (
-        <KillersPerYearLine
-          key={year}
-          year={year}
-          color={color}
-          height={height}
-          killers={killers}
-          is_selected={is_selected}
-          hovered_line={hovered_line}
-          set_hovered_line={set_hovered_line}
-          year_line_chunk={year_line_chunk}
-          i={i}
-        />
-      ))}
-    </Svg>
+    <Select
+      relative
+      id="blend_mode_select"
+      onClick={() => set_is_open(!is_open)}
+    >
+      <Options>
+        <SelectedOption style={{ height: '34px' }}>{blend_mode}</SelectedOption>
+        <Div mt10 pv10 b_rad10 bg_white none={!is_open}>
+          {blend_modes_list.map((mode, index) => (
+            <Option
+              key={mode}
+              bg_grey3={blend_mode === mode}
+              onMouseOver={() => set_blend_mode(mode)}
+            >
+              {mode}
+            </Option>
+          ))}
+        </Div>
+      </Options>
+    </Select>
   )
 }
 
-const KillersPerYearLine = (props) => {
-  const { year, killers, i, color, height, is_selected } = props
-  const { hovered_line, set_hovered_line, year_line_chunk } = props
-  const is_hovered = hovered_line.year === year
-  const line_coords_x = year_line_chunk * i
-  const line_height = killers.length * ((height - 75) / 100)
+const blend_modes_list = [
+  'exclusion',
+  'difference',
+  'overlay',
+  'multiply',
+  'screen',
+  'darken',
+  'color-dodge',
+  'color-burn',
+  'hard-light',
+  'soft-light',
+  'hue',
+  'saturation',
+  'color',
+  'luminosity',
+  'normal',
+]
 
-  return (
-    <Fragment>
-      <line
-        onMouseOver={() => set_hovered_line({ year, killers })}
-        x1={line_coords_x}
-        x2={line_coords_x}
-        y1={0}
-        y2={line_height}
-        strokeWidth={9}
-        stroke="transparent"
-      />
-      <line
-        style={{ filter: 'invert(100%)' }}
-        x1={line_coords_x}
-        x2={line_coords_x}
-        y1={0}
-        y2={line_height}
-        strokeWidth={(is_hovered && 9) || (is_selected && 2) || 1.25}
-        stroke={color}
-      />
-      {is_hovered && (
-        <Fragment>
-          <text fontSize={25} textAnchor="middle" x={line_coords_x} y={-45}>
-            {hovered_line.year}
-          </text>
-          <text
-            fontSize={25}
-            textAnchor="middle"
-            x={line_coords_x}
-            y={line_height + 60}
-          >
-            {hovered_line.killers.length}
-          </text>
-        </Fragment>
-      )}
-    </Fragment>
-  )
-}
-
-const current_year = new Date().getFullYear()
-
-const Wrapper = Component.c_crosshair.article()
-const Svg = Component.mh50.mh25__xs.mt75.of_visible.svg()
-const Year = Component.f_invert100.text()
-const Title = Component.fs2vw.fs22__xs.b50.b30__xs.l50.l30__xs.absolute.p()
+const Wrapper = Component.article()
+const Canvas = Component.t0.l0.absolute.canvas()
+const Input = Component.mv10.input()
+const Controls = Component.w140.absolute.l20.t20.div()
+const Button = Component.c_pointer.ba0.w100p.sans.pv10.b_rad20.button()
+const Label = Component.c_pointer.b_rad20.absolute.t0.l0.w100p.h100p.label()
+const Select =
+  Component.flex.ai_center.c_pointer.w100p.sans.ph10.ol_none.b_rad20.div()
+const Options = Component.b_rad10.zi1.w100p.absolute.t0.l0.div()
+const SelectedOption = Component.flex.ai_center.jc_center.div()
+const Option = Component.w100p.h20.flex.ai_center.jc_center.ellipsis.span()
+const Downloading =
+  Component.f_invert100.text_center.fs28.lh35.w100p.h100p.flex.ai_center.jc_center.div()
--- src/blocks/Block_38.js
+++ src/blocks/Block_38.js
@@ -1,188 +1,84 @@
 import { useState, useEffect } from 'react'
-import { random, get_invert_color } from '../utils/toolbox'
+import { random } from '../utils/toolbox'
 import { Component } from '../utils/flags'
-import { Arrow } from '../icons'
 
-export const Block_38 = ({ hovered, color }) => {
-  //states hooks
-  const [has_started, set_has_started] = useState(false)
+export const Block_38 = ({ color, is_selected, hovered }) => {
+  const [count, set_count] = useState(random(3, 7))
   const [wrapper, set_wrapper] = useState(null)
-  const [canvas, set_canvas] = useState(null)
-  const [context, set_context] = useState(null)
-  const [treasures, set_treasures] = useState([])
-  const [selected_color, set_selected_color] = useState('black')
-  const [{ x, y }, set_position] = useState({ x: 0, y: 0 })
+  const [dimensions, set_dimensions] = useState()
 
-  const finished = treasures.length && treasures.every((t) => t.found)
-
-  // init context and draw first items
   useEffect(() => {
-    if (!canvas) return
-    const context = canvas.getContext('2d')
-    set_context(context)
+    setTimeout(() => hovered && set_count(count < amount ? count + 1 : 1), 200)
+  })
 
-    const colors = generate_colors(color)
-    const treasures = generate_treasures(colors, canvas)
-    for (const treasure of treasures) draw_treasure(treasure, context)
-    set_treasures(treasures)
-
-    draw_avatar(context, 'black', canvas.width / 2, canvas.height)
-
-    set_position({ x: canvas.width / 2, y: canvas.height })
-  }, [canvas, color])
-
-  // redraw items with new coordinates & props on key moves
-  useEffect(() => {
-    if (!context || !hovered || !wrapper) return
-    const { width, height } = wrapper.getBoundingClientRect()
-    context.clearRect(0, 0, width, height) // optional
-
-    const visible_treasures = treasures.filter((t) => !t.found)
-    for (const treasure of visible_treasures) {
-      if (has_reached_treasure(x, y, treasure)) {
-        treasure.found = true
-        selected_color !== treasure.color && set_selected_color(treasure.color)
-        continue
-      }
-      draw_treasure(treasure, context)
-    }
-
-    draw_avatar(context, selected_color, x, y)
-  }, [context, wrapper, x, y, hovered, selected_color, treasures])
-
-  // focus canvas when the block is hovered; onfocus when not
   useEffect(() => {
     if (!wrapper) return
-    hovered ? wrapper.focus() : wrapper.blur()
-  }, [hovered, wrapper])
+    const resizeObserver = new ResizeObserver(() => {
+      set_dimensions(wrapper.getBoundingClientRect())
+    })
+    resizeObserver.observe(wrapper)
+    return () => resizeObserver.disconnect()
+  }, [wrapper])
 
   return (
-    <Wrapper
-      tabIndex="0"
-      elemRef={set_wrapper}
-      onKeyDown={(event) => {
-        event.preventDefault()
-        if (!arrow_keys.includes(event.key) || finished) return
-        !has_started && set_has_started(true)
-        event.key === 'ArrowDown' && set_position({ x, y: y + 2 })
-        event.key === 'ArrowUp' && set_position({ x, y: y - 2 })
-        event.key === 'ArrowLeft' && set_position({ x: x - 2, y })
-        event.key === 'ArrowRight' && set_position({ x: x + 2, y })
-      }}
-    >
-      {finished ? (
-        <Finished>
-          <Svg
-            viewBox="0 0 30 30"
-            width={wrapper.getBoundingClientRect().width / 1.5}
-            height={wrapper.getBoundingClientRect().height / 1.5}
-          >
-            <path fill={get_invert_color(color)} d={flower} />
-          </Svg>
-          <Congrats style={{ color: color.value }}>CONGRATS!</Congrats>
-        </Finished>
-      ) : (
-        <canvas
-          ref={set_canvas}
-          width={wrapper?.getBoundingClientRect().width}
-          height={wrapper?.getBoundingClientRect().height}
-        />
-      )}
-      {!has_started && (
-        <Start>
-          <Arrow
-            rotation={-90}
-            width={20}
-            stroke="black"
-            stroke_width={10}
-            mb5
-          />
-          <Span>Start</Span>
-        </Start>
-      )}
+    <Wrapper elemRef={set_wrapper} style={{ background: color.value }}>
+      <svg
+        viewBox={`0 0 ${size} ${size}`}
+        xmlns="http://www.w3.org/2000/svg"
+        width={dimensions?.width}
+        height={dimensions?.height}
+      >
+        {shapes.map((index) => (
+          <Shape key={index} index={index} count={count} color={color} />
+        ))}
+      </svg>
+      <Count fs4vw={is_selected} f_invert100 style={{ color: color.value }}>
+        {count}
+      </Count>
     </Wrapper>
   )
 }
 
-const draw_avatar = (context, color, x, y) => {
-  context.fillStyle = color
-  context.fillRect(
-    x - (radius * 1.5) / 2,
-    y - (radius * 1.5) / 2,
-    radius * 1.5,
-    radius * 1.5,
+const Shape = ({ index, count, color }) => {
+  const { x, y } = calc_coords(index)
+  const visible = index + 1 <= count
+
+  return (
+    <Svg
+      width={size / 2}
+      height={size / 2}
+      x={x - size / 2 / 2}
+      y={y - size / 2 / 2}
+      viewBox="0 0 1000 1000"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ filter: 'invert(100%)', mixBlendMode: 'difference' }}
+    >
+      <path
+        fill={visible ? 'black' : 'none'}
+        d="M600.28 77.53a117.76 117.76 0 0 0 127.54 52.82c85.34-20.3 162.13 56.49 141.83 141.83a117.76 117.76 0 0 0 52.82 127.54c74.71 46 74.71 154.57 0 200.56a117.76 117.76 0 0 0-52.82 127.54C890 813.16 813.16 890 727.82 869.65a117.76 117.76 0 0 0-127.54 52.82c-46 74.71-154.57 74.71-200.56 0a117.76 117.76 0 0 0-127.54-52.82C186.84 890 110.05 813.16 130.35 727.82a117.76 117.76 0 0 0-52.82-127.54c-74.71-46-74.71-154.57 0-200.56a117.76 117.76 0 0 0 52.82-127.54c-20.3-85.34 56.49-162.13 141.83-141.83a117.76 117.76 0 0 0 127.54-52.82c45.99-74.71 154.57-74.71 200.56 0Z"
+      />
+    </Svg>
   )
 }
 
-const draw_treasure = (treasure, context) => {
-  const { x, y, color } = treasure
-  const test = { x: x - radius, y: y - radius }
-  context.translate(test.x, test.y)
-  context.fillStyle = color
-  context.beginPath()
-  const path = new Path2D(flower)
-  context.fill(path)
-  context.translate(-test.x, -test.y)
+const calc_coords = (point) => {
+  const radians = 90 * (Math.PI / 180)
+  const theta = (Math.PI * 2) / shapes.length
+  const angle = theta * point - radians
+
+  const x = radius * Math.cos(angle) + center
+  const y = radius * Math.sin(angle) + center
+
+  return { x, y }
 }
 
-const has_reached_treasure = (x, y, treasure) =>
-  x >= treasure.x - radius * 1.5 &&
-  x <= treasure.x + radius * 1.5 &&
-  y >= treasure.y - radius * 1.5 &&
-  y <= treasure.y + radius * 1.5
+const size = 20
+const center = size / 2
+const radius = center
 
-const generate_colors = (color) =>
-  Array(2)
-    .fill()
-    .map((e, index) => {
-      const { hue, saturation } = color
-      const hue_inc = (index + 2) * 100
-      return `hsl(${hue + hue_inc}, ${100 - saturation}%, 50%)`
-    })
+const amount = 10
+const shapes = [...Array(amount).keys()]
 
-const generate_treasures = (colors, wrapper) =>
-  colors.flatMap((color) => {
-    const width = wrapper?.getBoundingClientRect().width
-    const height = wrapper?.getBoundingClientRect().height
-
-    let items = []
-    for (let i = 0; i < 3; i++) {
-      const x = random(50, width - 50)
-      const y = random(50, height - 50)
-      const item = { x, y, found: false, color }
-      items.push(item)
-    }
-    return items
-  })
-
-const flower = `
-M 18.39, 2.39 
-l.85, 2.38, 2.27, -1.08
-a 3.6, 3.6, 0, 0, 1, 4.8, 4.8
-l -1.08, 2.27, 2.38, 0.85
-a 3.6, 3.6, 0, 0, 1, 0, 6.78
-l -2.38, 0.85, 1.08, 2.27
-a 3.6, 3.6, 0, 0, 1, -4.8, 4.8
-l -2.27 -1.08 -0.85, 2.38
-a 3.6, 3.6, 0, 0, 1, -6.78, 0
-l -0.85, -2.38
-L 8.49, 26.31 
-a 3.6, 3.6, 0, 0, 1, -4.8, -4.8
-l 1.08, -2.27, -2.38, -0.85
-a 3.6, 3.6, 0, 0, 1, 0, -6.78
-l 2.38 -0.85
-L 3.69, 8.49
-a 3.6, 3.6, 0, 0, 1, 4.8 -4.8
-l 2.27, 1.08, 0.85, -2.38
-A 3.6, 3.6, 0, 0, 1, 18.39, 2.39
-Z`
-
-const radius = 15
-const arrow_keys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']
-
-const Wrapper = Component.article()
-const Finished = Component.w100p.h100p.flex.ai_center.jc_center.div()
-const Congrats = Component.fs35.absolute.div()
-const Svg = Component.absolute.svg()
-const Start = Component.absolute.b20.w100p.text_center.div()
-const Span = Component.mb5.div()
+const Wrapper = Component.flex.ai_center.jc_center.article()
+const Count = Component.absolute.div()
+const Svg = Component.f_invert100.blend_difference.svg()
