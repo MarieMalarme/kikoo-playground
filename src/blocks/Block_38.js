83acd94832df8b500935c7ac9e26112db11b6f72
Marie Malarme <malarmemarie@gmail.com>
2023-02-23 20:17:18 +0100

    block 38: grow a heart-with-hands emoji on mouse move

--- src/blocks/Block_38.js
+++ src/blocks/Block_38.js
@@ -1,6 +1,48 @@
 import { useState, useEffect } from 'react'
 import { Component } from '../utils/flags'
 
-export const Block_38 = ({ hovered, color }) => <Wrapper />
+export const Block_38 = ({ hovered, color }) => {
+  const [mouse_x, set_mouse_x] = useState(150)
+  const [wrapper, set_wrapper] = useState(null)
+  const [{ width, height }, set_dimensions] = useState({ width: 0, height: 0 })
 
-const Wrapper = Component.article()
+  useEffect(() => {
+    if (!wrapper) return
+    set_dimensions(wrapper.getBoundingClientRect())
+
+    const resizeObserver = new ResizeObserver(() => {
+      set_dimensions(wrapper.getBoundingClientRect())
+    })
+    resizeObserver.observe(wrapper)
+    return () => resizeObserver.disconnect()
+  }, [wrapper, height, width])
+
+  useEffect(() => {
+    const prevent_scroll = (event) => event.preventDefault()
+    if (!wrapper) return
+    wrapper.addEventListener('touchmove', prevent_scroll, { passive: false })
+  }, [wrapper])
+
+  const update_mouse_x = (event) => {
+    event = event.type === 'touchmove' ? event.touches[0] : event
+    const { offsetLeft } = wrapper.offsetParent
+    const client_x = event.clientX - offsetLeft
+    const allowed_x = client_x > limit && width - client_x > limit
+    if (allowed_x) set_mouse_x(client_x)
+  }
+
+  return (
+    <Wrapper
+      elemRef={set_wrapper}
+      onMouseMove={update_mouse_x}
+      onTouchMove={update_mouse_x}
+      style={{ fontSize: mouse_x / 1.25 }}
+    >
+      🫶
+    </Wrapper>
+  )
+}
+
+const limit = 50
+
+const Wrapper = Component.flex.ai_center.jc_center.article()
