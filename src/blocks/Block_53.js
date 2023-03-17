df7e39de6e053364f4e11811fa1648269e61e4e7
Marie Malarme <malarmemarie@gmail.com>
2023-03-17 12:36:20 +0100

    blocks: switch blocks 53 & 61

--- src/blocks/Block_53.js
+++ src/blocks/Block_53.js
@@ -1,133 +1,97 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { Component } from '../utils/flags'
-import { random } from '../utils/toolbox'
+import * as PIXI from 'pixi.js'
+import { Stage, Sprite, Container, withFilters } from '@inlet/react-pixi'
 
-export const Block_53 = ({ hovered }) => {
+import sprite from '../images/sprite.png'
+import displacement_map from '../images/displacement-1.jpg'
+
+export const Block_53 = ({ is_selected, hovered, color }) => {
   const [wrapper, set_wrapper] = useState(null)
-  const [has_clicked, set_has_clicked] = useState(null)
   const [mouse, set_mouse] = useState({ x: 100, y: 100 })
-  const [hsl, set_hsl] = useState({ hue: 140, luminosity: 50 })
-  const [{ width, height }, set_dimensions] = useState({ width: 0, height: 0 })
 
-  const { hue, luminosity } = hsl
-  const color = `hsl(${hue}, 50%, ${luminosity}%)`
-
-  useEffect(() => {
-    if (!wrapper) return
-    set_dimensions(wrapper.getBoundingClientRect())
-    set_mouse({
-      x: random(padding, width - padding),
-      y: random(padding, height - padding),
-    })
-
-    const resizeObserver = new ResizeObserver(() => {
-      set_dimensions(wrapper.getBoundingClientRect())
-      set_mouse({
-        x: random(padding, width - padding),
-        y: random(padding, height - padding),
-      })
-    })
-    resizeObserver.observe(wrapper)
-    return () => resizeObserver.disconnect()
-  }, [wrapper, height, width])
+  const update_mouse = (event) => {
+    event = event.type === 'touchmove' ? event.touches[0] : event
+    const { width, height } = wrapper.getBoundingClientRect()
+    // translate the mouse position in the page to the coordinate system of the block
+    const translator_x = width / 2 + wrapper.offsetParent.offsetLeft
+    const translator_y = height / 2 + wrapper.offsetParent.offsetTop
+    const x = event.pageX - translator_x
+    const y = event.pageY - translator_y
+    set_mouse({ x, y })
+  }
 
   useEffect(() => {
-    if (!wrapper) return
     const prevent_scroll = (event) => event.preventDefault()
+    if (!wrapper) return
     wrapper.addEventListener('touchmove', prevent_scroll, { passive: false })
-    return () => document.removeEventListener('touchmove', prevent_scroll)
   }, [wrapper])
 
   useEffect(() => {
-    const copy = (event) => {
-      event.preventDefault()
-      if (!event.clipboardData) return
-      event.clipboardData.setData('text/plain', color)
-    }
-
-    document.addEventListener('copy', copy)
-    return () => document.removeEventListener('copy', copy)
-  }, [color])
-
-  useEffect(() => {
-    clearTimeout(timeout_id)
-    timeout_id = setTimeout(() => set_has_clicked(false), 800)
-  }, [has_clicked])
-
-  const update_values = (event) => {
-    event = event.type === 'touchmove' ? event.touches[0] : event
-    const { offsetTop, offsetLeft } = wrapper.offsetParent
-    const client_x = event.clientX - offsetLeft
-    const client_y = event.clientY - (offsetTop - window.pageYOffset)
-    set_mouse({ x: client_x, y: client_y })
-
-    const allowed_x = client_x > padding && width - client_x > padding
-    const allowed_y = client_y > padding && height - client_y > padding
-
-    if (!allowed_y || !allowed_x) return
-
-    const translated_x = client_x - padding
-    const max_x = width - padding * 2
-    const hue = calc_interval_value(translated_x / max_x, 360)
-
-    const translated_y = client_y - padding
-    const max_y = height - padding * 2
-    const luminosity = calc_interval_value(translated_y / max_y, 100)
-
-    set_hsl({ ...hsl, hue, luminosity })
-  }
+    if (!wrapper) return
+    wrapper.focus()
+  }, [hovered])
 
   return (
     <Wrapper
+      onTouchMove={update_mouse}
+      onMouseMove={update_mouse}
       elemRef={set_wrapper}
-      onTouchMove={update_values}
-      onMouseMove={update_values}
-      onClick={() => {
-        document.execCommand('copy')
-        set_has_clicked(true)
-      }}
-      style={{ background: color, cursor: 'none' }}
     >
-      <Dot className="circle-pop-disappear" />
-      <Value t40 r40>
-        hue {hue}
-      </Value>
-      <Value b40 l40>
-        luminosity {luminosity}
-      </Value>
-      <LineHorizontal style={{ top: mouse.y }} />
-      <LineVertical style={{ left: mouse.x }} />
-      {has_clicked && (
-        <Cursor
-          className="circle-pop-disappear"
-          style={{ top: mouse.y - 5, left: mouse.x - 5 }}
+      <Stage>
+        <DisplacementMap
+          mouse={mouse}
+          width={wrapper?.getBoundingClientRect().width}
         />
-      )}
-      <Value
-        mono
-        fs12
-        ws_nowrap
-        c_none
-        style={{ left: mouse.x + 15, top: mouse.y + 10 }}
-      >
-        {has_clicked ? 'Copied to clipboard!' : 'Click to pick'}
-      </Value>
+      </Stage>
     </Wrapper>
   )
 }
 
-const calc_interval_value = (number, max) =>
-  Math.round(Math.min(max, Math.max(0, max * number)))
+const DisplacementMap = ({ mouse, width }) => {
+  const filter_ref = useRef()
+  const [render_filter, set_render_filter] = useState(false)
 
-let timeout_id
-const padding = 40
+  useEffect(() => {
+    filter_ref.current.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT
+    set_render_filter(true)
+  }, [])
 
-const Wrapper = Component.flex.ai_center.jc_center.article()
-const Value = Component.absolute.blend_difference.white.div()
-const Dot =
-  Component.h15.w15.l40.t40.absolute.b_rad50p.bg_white.blend_difference.div()
-const LineHorizontal =
-  Component.blend_difference.absolute.w100p.h1.bg_white.o50.div()
-const LineVertical =
-  Component.blend_difference.absolute.h100p.w1.bg_white.o50.div()
-const Cursor = Component.o0.absolute.blend_difference.b_rad50p.bg_white.div()
+  return (
+    <>
+      {/* displacement map image */}
+      <Sprite
+        anchor={0.25}
+        x={mouse.x}
+        y={mouse.y}
+        width={width * 2}
+        height={width * 2}
+        image={displacement_map}
+        ref={filter_ref}
+      />
+
+      {render_filter && (
+        <Filters
+          displacement={{
+            construct: [filter_ref.current],
+            scale: { x: 100, y: 100 },
+          }}
+        >
+          {/* base image to deform */}
+          <Sprite
+            y={-12}
+            width={width + 50}
+            height={width + 50}
+            image={sprite}
+          />
+        </Filters>
+      )}
+    </>
+  )
+}
+
+const Filters = withFilters(Container, {
+  displacement: PIXI.filters.DisplacementFilter,
+})
+
+const Wrapper = Component.flex.flex_wrap.article()
